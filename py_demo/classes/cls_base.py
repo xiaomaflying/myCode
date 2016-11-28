16826343d34113154a63b08ae8603a1a2827e324
maxinmin <maxinmin@b.360.cn>
2016-11-28 11:05:06 +0800

    add base_classes.py

---
+++ py_demo/classes/cls_base.py
@@ -0,0 +1,56 @@
+#encoding=utf-8
+
+"""
+一、基本介绍
+    1. 类也是一种对象；
+    2. 类是实现面向对象的工具，用来包装代码，更好的重用代码；
+    3. 定义类，并通过来生成类的实例（对象）
+
+二、类的定义：新式类和老式类
+    1. 定义了两种类，通过dir()用来对比
+    2. 结论：始终使用新式类
+
+三、用类生成对象
+    1. 类名+()的方式
+    2. __init__(self, ...)
+    3. 为类添加方法
+    4. __str__(self) 实现对象的可读性
+    5. 以双下划线__开头的方法是特殊的方法，以后还会继续介绍
+"""
+
+
+class OldCls:
+    pass
+
+class NewCls(object):
+    pass
+
+
+class Student(object):
+
+    def __init__(self, name, age=18):
+        self.name = name
+        self.age = age
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def __str__(self):
+        return 'name is %s, age is %s' % (self.name, self.age)
+
+
+def test1():
+    stu = Student('xiaoming')
+    print stu.name
+    print stu.age
+    #print stu
+
+    stu1 = Student('xiaohong', age=19)
+    print stu1.get_name()
+    print stu1.get_age()
+
+
+test1()
